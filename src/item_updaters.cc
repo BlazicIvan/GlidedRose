6f6175232c104aff32fa389944f99dde1b19bf45
Nada Telebakovic <nada.telebakovic@zuehlke.com>
2020-04-01 13:54:27 +0200

    Default Item Updater added
    Signed-off-by: Nada Telebakovic <nada.telebakovic@zuehlke.com>

--- src/item_updaters.cc
+++ src/item_updaters.cc
@@ -2,5 +2,13 @@
 
 void DefaultItemUpdater::updateItem(Item& item)
 {
-    
+    if (item.quality > 0)
+    {
+        item.quality -= 1;
+        item.sellIn -= 1;
+        if (item.sellIn < 0 && item.quality > 0)
+        {
+            item.quality -= 1;
+        }
+    }
 }
